9bf6710893d0872ae128382bc41b759bcca08490
RRyannis <137493198+RRyannis@users.noreply.github.com>
2025-10-22 11:50:08 +0300

    Minor tiny update, will add more tonight

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -3,7 +3,14 @@ import "./leftBar.scss";
 const LeftBar = () =>{
     return(
         <div className="leftBar">
-            LeftBar
+            <div className="container">
+                <div className="menun">
+                    <div className="user">
+                        <img src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg" alt="" />
+                        <span>Babis Vovos</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
