6a808dd8f7bc5e5227e25df6d823cc3128e7c0bd
RRyannis <137493198+RRyannis@users.noreply.github.com>
2025-10-28 18:45:57 +0200

    most of the frontend done, share feature to be added and then backend work

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -7,7 +7,7 @@ import iceShard from "../../assets/bookFrame1.jpg";
 const Posts = () => {
   //TEMPORARY
   const posts = [
-    { id:1, name: "John Dove", userId: 1, profilePic: "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600", desc: "Lovely quote from To Kill a Mockingbird", img: mockingbird }, { id:2, name: "Susan Johnson's Baby Shampoo", userId: 1, profilePic: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg", desc: "Intriguing quote from Sword of Destiny", img: iceShard }
+    { id:1, name: "John Dove", userId: 1, profilePic: "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600", desc: "Lovely quote from To Kill a Mockingbird", img: mockingbird }, { id:2, name: "Susan Johnson's Baby Shampoo", userId: 2, profilePic: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg", desc: "Intriguing quote from Sword of Destiny", img: iceShard }
   ];
 
   return <div className="posts">
